52bca5b348d389a88152f9d79d2c78d0b01af2ef
Jeevan Kumar <jeevankumarhari2002@gmail.com>
2024-06-19 19:03:14 +0530

    Displayed only 10 currencies in simplified view.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,7 +8,7 @@ import { Cryptocurrencies, News } from '../components'
 const {Title} = Typography;
 
 const HomePage = () => {
-  const {data, isFetching } = useGetCryptosQuery();
+  const {data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
   if (isFetching) return 'Loading...'
