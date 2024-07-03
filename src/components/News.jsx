3de1616e6817b4d4399af4c7c51db31ab35f09bb
Jeevan Kumar <jeevankumarhari2002@gmail.com>
2024-07-03 22:54:59 +0530

    Added more media queries.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -18,7 +18,7 @@ const News = ({ simplified }) => {
   return (
     <Row gutter={[24, 24]}>
       {renderNews.map((news, i) => (
-        <Col xs={24} sm={12} lg={8} key={i}>
+        <Col xs={24} sm={12} lg={12} key={i}>
           <Card hoverable className="news-card">
             <a href={news.source_url} target="_blank" rel="noreferrer">
               <div className="news-image-container">
